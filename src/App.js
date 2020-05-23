15d81cf4bc2a2a4e7ca4d301d38c69c9b68bded4
Mollie-S <olcha.olcha@gmail.com>
2020-05-23 21:15:25 +0200

    Timer display shows numbers decrementing every second. If session time is over break time is shown and vice versa.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React, {useState} from 'react';
-import Header from './components/Header';
-import Timer from './components/Timer';
-import TimerController from './components/TimerController';
-import './styles/style.scss';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Timer from "./components/Timer";
+import TimerController from "./components/TimerController";
+import "./styles/style.scss";
 
 function App() {
-  const [minutes, setMinutes] = useState(25);
-  const [seconds] = useState('00');
+  const [breakSeconds, setBreakSeconds] = useState(5);
+  const [sessionSeconds, setSessionSeconds] = useState(10);
+  const [seconds, setSeconds] = useState(sessionSeconds);
 
-  function incrementMinutes() {
-    setMinutes(prevState => prevState + 1)
+  const [isSession, setIsSession] = useState(true);
+
+  // function incrementMinutes() {
+  //   setMinutes(prevState => prevState + 1)
+  // }
+  // function decrementMinutes() {
+  //   setMinutes(prevState => prevState - 1)
+  // }
+  let timerLabel;
+  if (isSession) {
+    timerLabel = "Session";
+  } else {
+    timerLabel = "Break";
   }
-  function decrementMinutes() {
-    setMinutes(prevState => prevState - 1)
-  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds <= 0) {
+          setIsSession(!isSession);
+          return isSession ? breakSeconds : sessionSeconds;
+        } else {
+          return prevSeconds - 1;
+        }
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isSession, breakSeconds, sessionSeconds]);
 
   return (
     <div className="App">
       <Header />
-      <Timer minutes={minutes} seconds={seconds}/>
-      <div className='timer-controller'>
-        <TimerController 
-          controllerId='break' 
-          label='Break' 
-          length='5'
-          increment={incrementMinutes}
-          decrement={decrementMinutes}/>
-        <TimerController 
-          controllerId='session'
-          label='Session' 
-          length={minutes}
-          increment={incrementMinutes}
-          decrement={decrementMinutes}
-          />
+      <Timer seconds={seconds} timerLabel={timerLabel} />
+      <div className="timer-controller">
+        <TimerController
+          controllerId="break"
+          label="Break"
+          length={breakSeconds}
+          // increment={incrementMinutes}
+          // decrement={decrementMinutes}
+        />
+        <TimerController
+          controllerId="session"
+          label="Session"
+          length={sessionSeconds}
+          // increment={incrementMinutes}
+          // decrement={decrementMinutes}
+        />
       </div>
-     
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,18 +1,16 @@
-import React from 'react';
-
+import React from "react";
 
 function Timer(props) {
   return (
-    <div id='timer'>
-      <div id='timer-display'>
-        <div id='timer-label'>Session/Break</div>
-        <div id='time-left'>{props.minutes}:{props.seconds}</div>
+    <div id="timer">
+      <div id="timer-display">
+        <div id="timer-label">{props.timerLabel}</div>
+        <div id="time-left">{props.seconds}</div>
       </div>
-      <div className='timer-button'>
-        <button id='start_stop'>Start/Stop</button>
-        <button id='reset'>Reset</button>
+      <div className="timer-button">
+        <button id="start_stop">Start/Stop</button>
+        <button id="reset">Reset</button>
       </div>
-
     </div>
   );
 }
