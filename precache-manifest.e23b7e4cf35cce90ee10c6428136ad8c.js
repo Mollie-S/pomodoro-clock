884613bcf2aefb64a0d7be2f248016c39a821170
Mollie-S <olcha.olcha@gmail.com>
2020-05-26 14:55:47 +0200

    Updates

---
+++ precache-manifest.e23b7e4cf35cce90ee10c6428136ad8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a070efe2be248b5ce6a5b427f72e0560",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "2cd43534c598c1b2bc52",
+    "url": "/pomodoro-clock/static/css/main.f468c66f.chunk.css"
+  },
+  {
+    "revision": "c47430d0651337df9f98",
+    "url": "/pomodoro-clock/static/js/2.ffa170cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.ffa170cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd43534c598c1b2bc52",
+    "url": "/pomodoro-clock/static/js/main.1dd2be95.chunk.js"
+  },
+  {
+    "revision": "a4dcdb46a55158e82e38",
+    "url": "/pomodoro-clock/static/js/runtime-main.6695a290.js"
+  },
+  {
+    "revision": "5461ce5b38053fe8cfb9f0136d992e26",
+    "url": "/pomodoro-clock/static/media/TOWERCLO.5461ce5b.mp3"
+  }
+]);
